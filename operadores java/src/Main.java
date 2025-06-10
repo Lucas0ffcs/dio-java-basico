2402892937006cb97cb99d0f35997c62d61256e5
Lucas0ffcs <lacerdalucas511@gmail.com>
2025-06-10 11:17:29 -0300

    Adiciona codigo que testa operadores

---
+++ operadores java/src/Main.java
@@ -0,0 +1,22 @@
+// Operadores
+public class Main{
+    public static void main(String[] args) {
+        // && = verifica se duas ou mais condições verdadeiras
+        if(1==1 && 2==2){
+            System.out.println("Verdadeiro");
+        }
+        else{
+            System.out.println("Falso");
+        }
+        // || = ou, verifica se ao menos uma das condições é satisfeita
+        if(1==1 || 2==1){
+            System.out.println("Verdadeiro");
+        }
+        else{
+            System.out.println("falso");
+        }
+        // até o momento nao encontrei nenhum operador que funcione como o XOR presente em
+        // outras linguagens
+
+    }
+}
