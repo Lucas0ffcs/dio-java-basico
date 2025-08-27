fe7456b247227e1b213e76a7fdb410893d95585d
Lucas0ffcs <lacerdalucas511@gmail.com>
2025-08-27 20:16:04 -0300

    Adiciona exercícios de revisão

--- exe/src/somaimpar.java
+++ exe/src/somaimpar.java
@@ -1,2 +1,29 @@
+import java.util.Scanner;
+
 public class somaimpar {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+
+
+        System.out.println("Digite um número e veja a soma de seus ímpares e seus pares:");
+        String numero = scan.nextLine();
+
+
+        int par = 0;
+        int impar = 0;
+
+        for(char x : numero.toCharArray()){
+            if(Integer.parseInt(Character.toString(x))%2 == 0){
+                par += Integer.parseInt(Character.toString(x));
+            }else{
+                impar += Integer.parseInt(Character.toString(x));
+            }
+
+        }
+
+        System.out.println("A soma dos números pares é:"+ par);
+        System.out.println("E a soma dos números ímpares é:"+ impar);
+
+    }
 }
